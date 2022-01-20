8a0244ca4a714e8a1191c58a65a3f0d3be0aa7df
ko23yun <ko23yun@naver.com>
2022-01-20 20:44:51 +0900

    boj 10952 PASS

--- boj.js
+++ boj.js
@@ -1,1 +1,17 @@
-console.log(123);
+// add, commit, push 해주기!!! /dev/stdin
+let fs = require("fs");
+let input = fs.readFileSync("./data.txt").toString().trim().split("\n");
+
+let num = input.length - 1;
+
+let i = 0;
+let answer = "";
+
+while (i < num) {
+  let listNum = input[i].split(" ").map((v) => +v);
+  answer += listNum[0] + listNum[1] + "\n";
+
+  i++;
+}
+
+console.log(answer.trim());
