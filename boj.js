afe07c6015ee114afd5296d8e3ba5727486638ba
ko23yun <ko23yun@naver.com>
2022-01-25 20:41:00 +0900

    boj 4673 셀프넘버

--- boj.js
+++ boj.js
@@ -1,20 +1,24 @@
 // add, commit, push 해주기!!! /dev/stdin
 let fs = require("fs");
-let input = fs
-  .readFileSync("./data.txt")
-  .toString()
-  .split("\n")
-  .map((v) => +v);
+let input = fs.readFileSync("./data.txt").toString().trim().split("\n");
 
-let max = input[0];
-let count = 0;
-
-for (let i = 1; i < input.length; i++) {
-  if (max < input[i]) {
-    max = input[i];
-    count = i;
+const d = (n) => {
+  let num = String(n);
+  let nextNum = n;
+  for (let i = 0; i < num.length; i++) {
+    nextNum += Number(num[i]);
   }
+  return nextNum;
+};
+
+let dList = [];
+// 9973
+for (let i = 1; i < 9990; i++) {
+  dList.push(d(i));
 }
 
-console.log(max);
-console.log(count + 1);
+let dSortedList = dList.sort((a, b) => a - b);
+
+for (let i = 1; i < 10000; i++) {
+  if (!dSortedList.includes(i)) console.log(i);
+}
