6bb08265ef7e319e33e2438ea941f3da797f6ff3
ko23yun <ko23yun@naver.com>
2022-01-25 21:08:12 +0900

    boj 1065 한수

--- boj.js
+++ boj.js
@@ -1,24 +1,24 @@
 // add, commit, push 해주기!!! /dev/stdin
 let fs = require("fs");
-let input = fs.readFileSync("./data.txt").toString().trim().split("\n");
+let input = fs.readFileSync("./data.txt").toString().trim();
 
-const d = (n) => {
-  let num = String(n);
-  let nextNum = n;
-  for (let i = 0; i < num.length; i++) {
-    nextNum += Number(num[i]);
+// 1000 이하의 정수
+// 1 ~ 9 -> 등차수열
+// 10 ~ 99 -> 등차수열
+// 100 ~ 999 -> 100의 자리, 10의 자리, 1의 자리 숫자들이 등차수열인지 확인 필요
+
+let getNumber = Number(input);
+let answer = getNumber;
+
+let checkNumber = (num) => {
+  if (num <= 99) {
+    return;
+  } else if (num >= 100) {
+    let numString = String(num);
+    let subtract = numString[0] - numString[1] == numString[1] - numString[2];
+    for (let i = 100; i <= num; i++) {
+      if (subtract) answer++;
+    }
   }
-  return nextNum;
+  console.log(answer);
 };
-
-let dList = [];
-// 9973
-for (let i = 1; i < 9990; i++) {
-  dList.push(d(i));
-}
-
-let dSortedList = dList.sort((a, b) => a - b);
-
-for (let i = 1; i < 10000; i++) {
-  if (!dSortedList.includes(i)) console.log(i);
-}
